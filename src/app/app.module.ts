509df6b56229ee46548c34cfd20d773817a45b73
StacyChebet <staceychebet23@gmail.com>
2018-10-28 18:08:27 +0300

    generate component for user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
